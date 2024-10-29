1954b3cbd7c4edf91dfc36e3f6f9ed23265403ee
esc <escluse@pm.me>
2024-10-29 17:50:12 -0400

    [chore] version bump

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <div class="max-w-lg 3xl:max-w-xl">
 	<h1 class="font-display text-4xl">
-		<span class="text-neon-cyan">
-			Minimal SvelteKit + Tailwind configuration using
-		</span>
-		<span class="text-neon-pink">Deno!</span>
+		Minimal SvelteKit + Tailwind configuration using
+		<span class="text-neon-red">Deno!</span>
 	</h1>
 </div>
