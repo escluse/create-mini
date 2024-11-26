2f11f8d33a33ab736220399345d53f614074d10f
esc <escluse@pm.me>
2024-11-26 00:06:13 -0500

    [style] main content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<div class="max-w-lg 3xl:max-w-xl">
-	<h1 class="font-display text-4xl">
-		Minimal SvelteKit + Tailwind configuration using
+<div class="border-2">
+	<h1 class="m-2 font-mono text-2xl">
+		Minimal SvelteKit + Tailwind config using
 		<span class="text-neon-red">Deno!</span>
 	</h1>
 </div>
